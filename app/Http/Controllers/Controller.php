62f00c54a689071253e7cbe0ddcf5c747dac17aa
MuhammadAthalla <juraganathalla@gmail.com>
2023-06-07 20:37:46 +0700

    hfasoga

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,7 +12,7 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     public function wow (){
-        return view('');
+        return view('sdfuasfa');
     }
 
 }
