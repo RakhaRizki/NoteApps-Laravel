8493d2289f5019e686e13c89273238d764ad5e27
rakha <babanrakha@gmail.com.com>
2023-06-07 20:38:23 +0700

    m

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,6 +12,7 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     public function wow (){
-        
+        return view('');
     }
+
 }
