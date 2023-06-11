a424d7b1d3d93f31cdc1f3e28035332669fc6529
rakha <babanrakha@gmail.com.com>
2023-06-11 20:33:11 +0700

    add views

---
+++ resources/views/layouts/app/footer.blade.php
@@ -0,0 +1,9 @@
+
+<div class="container">
+
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <p class="col-md-4 mb-0 text-muted">© 2023 IDNBS, Corp</p>
+    <i class="fa-solid fa-dragon fs-5 me-3"> NotesApps </i>
+</footer>
+
+</div>
