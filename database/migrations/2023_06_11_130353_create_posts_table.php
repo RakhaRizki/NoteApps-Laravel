3c61d185141f038f50ac1849b11d1bd0a27fe9ee
rakha <babanrakha@gmail.com.com>
2023-06-15 20:12:53 +0700

    new

--- database/migrations/2023_06_11_130353_create_posts_table.php
+++ database/migrations/2023_06_11_130353_create_posts_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
 
             $table->id();
-            $table->string('title');
+            $table->string('title')->nullable();
             $table->longText('deskripsi');
             $table->timestamps();
 
